f7deab9dfc42d4dd64c10f1bbc2c07682439f307
HaxtorMoogle <haxtormoogle@gmail.com>
2011-11-10 11:46:01 -0500

    debug fun code added and config basework

--- src/me/daddychurchill/CityWorld/CityWorld.java
+++ src/me/daddychurchill/CityWorld/CityWorld.java
@@ -1,14 +1,27 @@
 package me.daddychurchill.CityWorld;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CityWorld extends JavaPlugin{
 
-	protected Logger log = Logger.getLogger("Minecraft");
-	
+
+	/**
+     * Logger for debugging.
+     */
+    public static final Logger log = Logger.getLogger("Minecraft.CityWorld");
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+    
+   	protected String pluignDirPath;
+   	protected File configFile;
+   	
 	@Override
 	public void onDisable() {
 		log.info("CityWorld has been disabled");
@@ -17,9 +30,32 @@ public class CityWorld extends JavaPlugin{
 
 	@Override
 	public void onEnable() {
-		log.info("CityWorld has been enabled");
-		
+		pluignDirPath = getDataFolder().getAbsolutePath();
+		configFile = new File(pluignDirPath + File.separator + "config.yml");
+
+		PluginManager pm = getServer().getPluginManager();
+		getCommand("debug").setExecutor(new CityWorldDebugCMD(this));
+
+		PluginDescriptionFile pdfFile = this.getDescription();
+		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+
 	}
+	public boolean isDebugging(final Player player) 
+	{
+        if (debugees.containsKey(player))
+        {
+            return debugees.get(player);
+        } 
+        else 
+        {
+            return false;
+        }
+    }
+
+    public void setDebugging(final Player player, final boolean value) 
+    {
+        debugees.put(player, value);
+    }
 
 	
 	@Override
