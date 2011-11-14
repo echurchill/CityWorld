57e40e0aef2384fb132b74918b191bd5531947e2
HaxtorMoogle <haxtormoogle@gmail.com>
2011-11-14 11:54:42 -0500

    missing files

---
+++ src/me/daddychurchill/CityWorld/CityWorldConfig.java
@@ -0,0 +1,13 @@
+package me.daddychurchill.CityWorld;
+
+import java.io.File;
+
+public class CityWorldConfig 
+{
+
+	public CityWorldConfig(File configFile)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
