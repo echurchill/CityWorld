47d78b8dff8970e4db5ab6db7130427fe81ce143
Eddie Churchill <eddie@virtualchurchill.com>
2012-06-25 11:30:30 -0700

    Added Leave as an option to /CityWorld... it will let "noobs" go back to the default world

--- src/me/daddychurchill/CityWorld/CityWorldCreateCMD.java
+++ src/me/daddychurchill/CityWorld/CityWorldCreateCMD.java
@@ -1,5 +1,7 @@
 package me.daddychurchill.CityWorld;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,9 +20,52 @@ public class CityWorldCreateCMD implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			if (player.hasPermission("cityworld.command")) {
-				player.sendMessage("Loading/creating CityWorld... This might take a moment...");
-				player.teleport(plugin.getCityWorld().getSpawnLocation());
-				return true;
+				boolean leaving = false;
+				boolean error = false;
+				
+				// arguments?
+				if (split.length > 0) {
+					if (split[0].compareToIgnoreCase("LEAVE") == 0)
+						leaving = true;
+					else 
+						error = true;
+				}
+				
+				// that isn't an option we support
+				if (error) {
+					sender.sendMessage("Syntax error");
+					return false;
+					
+				// let's try to leave the city
+				} else if (leaving) {
+					World world = Bukkit.getServer().getWorld("world");
+					if (world == null) {
+						sender.sendMessage("Cannot find the default world");
+						return false;
+					} else if (player.getLocation().getWorld() == world) {
+						sender.sendMessage("You are already there");
+						return true;
+					} else {
+						player.sendMessage("Leaving CityWorld... Come back soon!");
+						player.teleport(world.getHighestBlockAt(world.getSpawnLocation()).getLocation());
+						return true;
+					}
+				
+				// ok let's enter the city
+				} else {
+					World world = plugin.getCityWorld();
+					if (world == null) {
+						sender.sendMessage("Cannot find the default CityWorld");
+						return false;
+					} else if (player.getLocation().getWorld() == world) {
+						sender.sendMessage("You are already there");
+						return true;
+					} else {
+						player.sendMessage("Loading/creating CityWorld... This might take a moment...");
+						player.teleport(plugin.getCityWorld().getSpawnLocation());
+						return true;
+					}
+				}
 			} else {
 				sender.sendMessage("You do not have permission to use this command");
 				return false;
