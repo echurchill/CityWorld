94228e64064dce68b752e83bf2cc4984728c348e
adept <adept@grayswandir.home.lan>
2012-07-06 15:44:54 -0500

    Added tekkit ores.  Still working on distribution. Not sure of the best way to do grown rubber trees; added planted rubber saplings instead.

--- src/me/daddychurchill/CityWorld/Plats/PlatLot.java
+++ src/me/daddychurchill/CityWorld/Plats/PlatLot.java
@@ -655,14 +655,53 @@ public abstract class PlatLot {
 	private static final int[] ore_minY = new int[]          {     8,    40,    16,    16,     5,     5,     8,     1};
 	private static final boolean[] ore_upper = new boolean[] {  true, false,  true,  true,  true,  true,  true, false};
 	
+	private static final int[] ore_types_tekkit = new int[] {Material.WATER.getId(),
+													  Material.GRAVEL.getId(), 
+													  Material.COAL_ORE.getId(),
+													  Material.IRON_ORE.getId(), 
+													  Material.GOLD_ORE.getId(), 
+													  Material.LAPIS_ORE.getId(),
+													  Material.REDSTONE_ORE.getId(),
+													  Material.DIAMOND_ORE.getId(),
+													  140, //ruby
+													  1401, // emerald
+													  1402, //sapphire
+													  1403, //silver
+													  1404, //tin
+													  1405, //copper
+													  1406, //tungsten
+													  1407, //nikolite
+													  142, //marble
+													  1421, //basalt
+													  247 //uranium
+													  };
+	//                                                   		      WATER   GRAV   COAL   IRON   GOLD  LAPIS  REDST   DIAM   RUBY   EMER   SAPP   SILV    TIN  COPPR   TUNG   NIKO   MARB   BASA   URAN  
+	private static final int[] ore_iterations_tekkit = new int[]    {     4,    20,    15,     6,     2,     2,     4,     1,     1,     1,     1,     3,     6,     6,     2,     2,    30,    20,     1};
+	private static final int[] ore_amountToDo_tekkit = new int[]    {     1,     6,     8,     6,     3,     2,     4,     2,     2,     2,     2,     4,     6,     6,     3,     8,    20,     8,     1};
+	private static final int[] ore_maxY_tekkit = new int[]          {   128,    96,   128,    68,    34,    30,    17,    16,    16,    16,    16,    34,    68,    96,    34,    68,   128,    10,    16};
+	private static final int[] ore_minY_tekkit = new int[]          {     8,    40,    16,    16,     5,     5,     8,     1,     2,     2,     2,     5,    16,    16,     2,     2,    40,     1,     2};
+	private static final boolean[] ore_upper_tekkit = new boolean[] {  true, false,  true,  true,  true,  true,  true, false, false, false, false,  true,  true,  true,  true,  true,  true, false, false};
+	
 	public void sprinkleOres(WorldGenerator generator, RealChunk chunk, Random random) {
-		for (int typeNdx = 0; typeNdx < ore_types.length; typeNdx++) {
-			int range = ore_maxY[typeNdx] - ore_minY[typeNdx];
-			for (int iter = 0; iter < ore_iterations[typeNdx]; iter++) {
-				sprinkleOres_iterate(generator, chunk, random, random.nextInt(16), random.nextInt(range) + ore_minY[typeNdx], random.nextInt(16), ore_amountToDo[typeNdx], ore_types[typeNdx]);
-				if (ore_upper[typeNdx])
-					sprinkleOres_iterate(generator, chunk, random, random.nextInt(16), 
-							(generator.seaLevel + generator.landRange) - ore_minY[typeNdx] - random.nextInt(range), random.nextInt(16), ore_amountToDo[typeNdx], ore_types[typeNdx]);
+		if (generator.settings.tekkitServer) {
+			for (int typeNdx = 0; typeNdx < ore_types_tekkit.length; typeNdx++) {
+				int range = ore_maxY_tekkit[typeNdx] - ore_minY_tekkit[typeNdx];
+				for (int iter = 0; iter < ore_iterations_tekkit[typeNdx]; iter++) {
+					sprinkleOres_iterate(generator, chunk, random, random.nextInt(16), random.nextInt(range) + ore_minY_tekkit[typeNdx], random.nextInt(16), ore_amountToDo_tekkit[typeNdx], ore_types_tekkit[typeNdx]);
+					if (ore_upper_tekkit[typeNdx])
+						sprinkleOres_iterate(generator, chunk, random, random.nextInt(16), 
+								(generator.seaLevel + generator.landRange) - ore_minY_tekkit[typeNdx] - random.nextInt(range), random.nextInt(16), ore_amountToDo_tekkit[typeNdx], ore_types_tekkit[typeNdx]);
+				}
+			}
+		} else {
+			for (int typeNdx = 0; typeNdx < ore_types.length; typeNdx++) {
+				int range = ore_maxY[typeNdx] - ore_minY[typeNdx];
+				for (int iter = 0; iter < ore_iterations[typeNdx]; iter++) {
+					sprinkleOres_iterate(generator, chunk, random, random.nextInt(16), random.nextInt(range) + ore_minY[typeNdx], random.nextInt(16), ore_amountToDo[typeNdx], ore_types[typeNdx]);
+					if (ore_upper[typeNdx])
+						sprinkleOres_iterate(generator, chunk, random, random.nextInt(16), 
+								(generator.seaLevel + generator.landRange) - ore_minY[typeNdx] - random.nextInt(range), random.nextInt(16), ore_amountToDo[typeNdx], ore_types[typeNdx]);
+				}
 			}
 		}
 	}
@@ -741,7 +780,13 @@ public abstract class PlatLot {
 		if (random.nextDouble() < 0.35) {
 			Block block = chunk.getActualBlock(x, y, z);
 			if (block.getTypeId() == stoneId) {
-				block.setTypeId(typeId, physics);
+				if (typeId > 1000) {
+					int blockId = typeId/10;
+					byte dataVal = (byte) (typeId%10);
+					block.setTypeIdAndData(blockId, dataVal, physics);
+				} else {
+					block.setTypeId(typeId, physics);
+				}
 				return true;
 			}
 		}
@@ -810,6 +855,8 @@ public abstract class PlatLot {
 						if (includeTrees && primary > 0.97 && x > 2 && x < 14 && z > 2 && z < 14) {
 							if (secondary > 0.90 && x > 5 && x < 11 && z > 5 && z < 11)
 								chunk.world.generateTree(chunk.getBlockLocation(x, y + 1, z), TreeType.BIG_TREE);
+							else if (secondary > 0.70 && generator.settings.tekkitServer)
+								chunk.setBlock(x, y + 1, z, 241,(byte) 0);
 							else if (secondary > 0.50)
 								chunk.world.generateTree(chunk.getBlockLocation(x, y + 1, z), TreeType.BIRCH);
 							else 
