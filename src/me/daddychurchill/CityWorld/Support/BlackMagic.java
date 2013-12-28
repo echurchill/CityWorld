b63b3cdca38db3e5f26e810717cfad56cf11fd35
echurchill <eddie@virtualchurchill.com>
2013-12-28 11:35:20 -0800

    Ditto
    ...it compiles and runs... that is all right now

--- src/me/daddychurchill/CityWorld/Support/BlackMagic.java
+++ src/me/daddychurchill/CityWorld/Support/BlackMagic.java
@@ -2,6 +2,7 @@ package me.daddychurchill.CityWorld.Support;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 
 @SuppressWarnings("deprecation")
 public abstract class BlackMagic {
@@ -30,22 +31,30 @@ public abstract class BlackMagic {
 	}
 	
 	public static final void setBlockType(Block block, int typeId, int data) {
-		if (data == 0)
-			block.setTypeId(typeId);
-		else
-			block.setTypeIdAndData(typeId, (byte) data, false); // No physics please
+		BlockState state = block.getState();
+		state.setTypeId(typeId);
+		if (data != 0)
+			state.setRawData((byte) data);
+		state.update(false, false); // no physics please
 	}
 	
 	public static final void setBlockType(Block block, Material material, int data) {
-		setBlockType(block, getMaterialId(material), data); // No physics please
+		BlockState state = block.getState();
+		state.setType(material);
+		if (data != 0)
+			state.setRawData((byte) data);
+		state.update(false, false);
 	}
 	
 	public static final void setBlock(RealChunk chunk, int x, int y, int z, Material material, int data) {
-		setBlockType(chunk.getActualBlock(x, y, z), getMaterialId(material), data);
+		setBlockType(chunk.getActualBlock(x, y, z), material, data);
 	}
 	
 	public static final void setBlocks(RealChunk chunk, int x, int y1, int y2, int z, Material material, int data) {
-		setBlocks(chunk, x, y1, y2, z, getMaterialId(material), data);
+//		CityWorld.log.info(String.format("Material = %s", material));
+		for (int y = y1; y < y2; y++) {
+			setBlockType(chunk.getActualBlock(x, y, z), material, data);
+		}
 	}
 	
 	public static final void setBlocks(RealChunk chunk, int x, int y1, int y2, int z, int typeId, int data) {
@@ -55,7 +64,11 @@ public abstract class BlackMagic {
 	}
 	
 	public static final void setBlocks(RealChunk chunk, int x1, int x2, int y, int z1, int z2, Material material, int data) {
-		setBlocks(chunk, x1, x2, y, z1, z2, getMaterialId(material), data);
+		for (int x = x1; x < x2; x++) {
+			for (int z = z1; z < z2; z++) {
+				setBlockType(chunk.getActualBlock(x, y, z), material, data);
+			}
+		}
 	}
 	
 	public static final void setBlocks(RealChunk chunk, int x1, int x2, int y, int z1, int z2, int typeId, int data) {
@@ -67,7 +80,13 @@ public abstract class BlackMagic {
 	}
 	
 	public static final void setBlocks(RealChunk chunk, int x1, int x2, int y1, int y2, int z1, int z2, Material material, int data) {
-		setBlocks(chunk, x1, x2, y1, y2, z1, z2, getMaterialId(material), data);
+		for (int x = x1; x < x2; x++) {
+			for (int y = y1; y < y2; y++) {
+				for (int z = z1; z < z2; z++) {
+					setBlockType(chunk.getActualBlock(x, y, z), material, data);
+				}
+			}
+		}
 	}
 	
 	public static final void setBlocks(RealChunk chunk, int x1, int x2, int y1, int y2, int z1, int z2, int typeId, int data) {
--- src/me/daddychurchill/CityWorld/Support/RealChunk.java
+++ src/me/daddychurchill/CityWorld/Support/RealChunk.java
@@ -2,6 +2,7 @@ package me.daddychurchill.CityWorld.Support;
 
 import me.daddychurchill.CityWorld.WorldGenerator;
 import org.bukkit.Chunk;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 public final class RealChunk extends SupportChunk {
@@ -27,4 +28,16 @@ public final class RealChunk extends SupportChunk {
 				getActualBlock(x, y, z - 1).isEmpty() && 
 				getActualBlock(x, y, z + 1).isEmpty());
 	}
+	
+	@Deprecated
+	public void setBlocks(int x, int y1, int y2, int z, Material primary, Material secondary, MaterialFactory maker) {
+//		setBlocks(x, y1, y2, z, BlackMagic.getMaterialId(primary), BlackMagic.getMaterialId(secondary), maker);
+	}
+
+	@Deprecated
+	public void setBlocks(int x1, int x2, int y1, int y2, int z1, int z2, Material primary, Material secondary, MaterialFactory maker) {
+//		setBlocks(x1, x2, y1, y2, z1, z2, BlackMagic.getMaterialId(primary), BlackMagic.getMaterialId(secondary), maker);
+	}
+
+	
 }
